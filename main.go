e0ae5a55661cb2f27147ab8e7d7d64e931487000
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-05-09 21:54:37 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type wordResult struct {
+	Word string `json:"word"`
+}
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", word)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":80"))
+}
+
+// Handler
+func word(c echo.Context) error {
+	word := &wordResult{Word: "cat"}
+	return c.JSON(http.StatusOK, word)
+}
